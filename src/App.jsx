8cd5ba7c777ca36ea02cc61ddc0653f0f848ae00
Emmanuel Achimugu Ikeleji <ikeleji@gmail.com>
2024-06-20 17:13:13 +0100

    Routes and Pages Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home'; 
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 export default function App() {
   return (
-    <div className='text-green-600 '>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/sign-in' element={<SignIn/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
